f9498fd08caeec574ac45e661a2f1a447c45a221
Evgenia Larina <larsik29@yandex.ru>
2024-06-29 23:52:37 +0300

    README.md changed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Стоимость билета в рублях
+        int ticketPrice = 13676;
+
+        // Количество бонусных миль за каждые 20 рублей
+        int milesPer20Rub = 1;
+
+        // Вычисление количества бонусных миль
+        int bonusMiles = ticketPrice / 20 * milesPer20Rub;
+
+        // Вывод результата
+        System.out.println("За билет стоимостью " + ticketPrice + " рублей начисляется " + bonusMiles + " бонусных миль.");
     }
 }
